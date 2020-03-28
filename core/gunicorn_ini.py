d5cd7c4706c872ef56866cccb1c9674417cd109b
Brigido Alejo <brigsalejo@gmail.com>
2020-03-28 20:39:25 +0800

    started development

--- core/gunicorn_ini.py
+++ core/gunicorn_ini.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# File  : gunicorn_ini.py
-# Author: Matthieu Petiteau <mpetiteau.pro@gmail.com>
-# Date  : 27.12.2019
-
-"""Gunicorn config."""
 import multiprocessing
 
 bind = "0.0.0.0:5000"
