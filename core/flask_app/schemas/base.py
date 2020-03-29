3b7645405626a0f7bee809ed537ed30a0981ae9c
Brigido Alejo <brigsalejo@gmail.com>
2020-03-29 11:40:41 +0800

    graphql query is working

--- core/flask_app/schemas/base.py
+++ core/flask_app/schemas/base.py
@@ -1,31 +1,16 @@
 from graphene_sqlalchemy import SQLAlchemyConnectionField
-import graphene
-from .planets import (Planet, CreatePlanet, UpdatePlanet)
-from .people import (People, CreatePerson, UpdatePerson)
+from graphene import ObjectType, String, Schema
+from .planets import (Planet, Query as QueryPlanets)
+from .people import (People, Query as QueryPeople)
 
 
-class Query(graphene.ObjectType):
-    """Nodes which can be queried by this API."""
-    node = graphene.relay.Node.Field()
 
-    # People
-    people = graphene.relay.Node.Field(People)
-    peopleList = SQLAlchemyConnectionField(People)
-
-    # Planets
-    planets = graphene.relay.Node.Field(Planet)
-    planetList = SQLAlchemyConnectionField(Planet)
+class Query(
+    QueryPlanets,
+    QueryPeople,
+    ObjectType
+):
+    pass
 
 
-class Mutation(graphene.ObjectType):
-    """Mutations which can be performed by this API."""
-    # Person mutation
-    createPerson = CreatePerson.Field()
-    updatePerson = UpdatePerson.Field()
-
-    # Planet mutations
-    createPlanet = CreatePlanet.Field()
-    updatePlanet = UpdatePlanet.Field()
-
-
-schema = graphene.Schema(query=Query, mutation=Mutation)
+schema = Schema(query=Query)
--- core/flask_app/schemas/people.py
+++ core/flask_app/schemas/people.py
@@ -6,71 +6,15 @@ from flask_app.utils import input_to_dictionary
 import graphene
 
 
-# Create a generic class to mutualize description of people attributes for both queries and mutations
-class PeopleAttribute:
-    name = graphene.String(description="Name of the person.")
-    height = graphene.String(description="Height of the person.")
-    mass = graphene.String(description="Mass of the person.")
-    hair_color = graphene.String(description="Hair color of the person.")
-    skin_color = graphene.String(description="Skin color of the person.")
-    eye_color = graphene.String(description="Eye color of the person.")
-    birth_year = graphene.String(description="Birth year of the person.")
-    gender = graphene.String(description="Gender of the person.")
-    planet_id = graphene.ID(description="Global Id of the planet from which the person comes from.")
-    url = graphene.String(description="URL of the person in the Star Wars API.")
-
-
 class People(SQLAlchemyObjectType):
     """People node."""
 
     class Meta:
         model = PeopleModel
-        interfaces = (graphene.relay.Node,)
 
 
-class CreatePersonInput(graphene.InputObjectType, PeopleAttribute):
-    """Arguments to create a person."""
-    pass
+class Query(graphene.ObjectType):
+    people = graphene.List(People)
 
-
-class CreatePerson(graphene.Mutation):
-    """Mutation to create a person."""
-    person = graphene.Field(lambda: People, description="Person created by this mutation.")
-
-    class Arguments:
-        input = CreatePersonInput(required=True)
-
-    def mutate(self, info, input):
-        data = input_to_dictionary(input)
-        data['created'] = datetime.utcnow()
-        data['edited'] = datetime.utcnow()
-
-        person = PeopleModel(**data)
-        db.session.add(person)
-        db.session.commit()
-
-        return CreatePerson(person=person)
-
-
-class UpdatePersonInput(graphene.InputObjectType, PeopleAttribute):
-    """Arguments to update a person."""
-    id = graphene.ID(required=True, description="Global Id of the person.")
-
-
-class UpdatePerson(graphene.Mutation):
-    """Update a person."""
-    person = graphene.Field(lambda: People, description="Person updated by this mutation.")
-
-    class Arguments:
-        input = UpdatePersonInput(required=True)
-
-    def mutate(self, info, input):
-        data = input_to_dictionary(input)
-        data['edited'] = datetime.utcnow()
-
-        person = db.session.query(PeopleModel).filter_by(id=data['id'])
-        person.update(data)
-        db.session.commit()
-        person = db.session.query(PeopleModel).filter_by(id=data['id']).first()
-
-        return UpdatePerson(person=person)
+    def resolve_people(self, info):
+        return PeopleModel.query.all()
