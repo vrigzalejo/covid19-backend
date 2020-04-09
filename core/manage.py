a8b31366e16bef742951e6fb2537c5421d92cd71
Brigido Alejo <brigsalejo@gmail.com>
2020-04-10 05:59:23 +0800

    seeding of countries is working

--- core/manage.py
+++ core/manage.py
@@ -7,8 +7,6 @@ import pandas as pd
 engine = db.get_engine()
 manager = Manager(app)
 
-print(db.engine.table_names())
-
 class Seeder(Command):
     covid19_path = '../COVID-19'
     ccse_confirmed_global_path = covid19_path + \
@@ -17,7 +15,20 @@ class Seeder(Command):
     def run(self):
         csv = self.ccse_confirmed_global_path + '/time_series_covid19_confirmed_global.csv'
         df = pd.read_csv(csv)
-        print(df.head(3))
+        filtered_df = df[['Country/Region', 'Lat', 'Long']]
+        filtered_df.index = filtered_df.index + 1
+        table_cols = ['country', 'latitude', 'longitude']
+        (
+            filtered_df.rename(columns=dict(zip(filtered_df.columns, table_cols)))
+            .to_sql(
+                con=engine,
+                name='countries',
+                if_exists='replace',
+                index=True,
+                index_label='id'
+            )
+        )
+        print(filtered_df.to_csv())
 
 if __name__ == "__main__":
     manager.add_command('seed', Seeder())
